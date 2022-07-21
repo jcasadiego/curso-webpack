ea8753f818e74943bde9fb1a27e38e4944c15ba6
jcasadiego <jcasadiego.a@gmail.com>
2022-07-21 06:38:45 -0400

    instalando y config babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,19 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                // Test declara que extensión de archivos aplicara el loader
+                test: /\.m?js$/,
+                // Exclude permite omitir archivos o carpetas especificas
+                exclude: /node_modules/,
+                // Use es un arreglo u objeto donde dices que loader aplicaras
+                use: {
+                    loader: "babel-loader"
+                },
+            }
+        ]
     }
 }
